fbbe7935ba2602ea74dec5dae0259150517bab12
Kim JinSeon <kwlstjs00@gmail.com>
2025-03-19 16:36:09 +0900

    feat: 사용자 찾기 오류 코드 추가

--- server/src/main/java/com/wordrelay/server/common/exception/ErrorCode.java
+++ server/src/main/java/com/wordrelay/server/common/exception/ErrorCode.java
@@ -1,5 +1,6 @@
 package com.wordrelay.server.common.exception;
 
+import javax.management.loading.MLetContent;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -16,7 +17,8 @@ public enum ErrorCode {
   PLAYER_NOT_FOUND(404, "플레이어를 찾을 수 없습니다."),
   UNAUTHORIZED_MOVE(403, "잘못된 차례입니다."),
   SERVER_ERROR(500, "서버 오류가 발생했습니다."),
-  BROWSER_ID_MISSING(400, "브라우저 ID가 누락되었습니다.");
+  BROWSER_ID_MISSING(400, "브라우저 ID가 누락되었습니다."),
+  USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다.");
 
   private final int code;
   private final String message;
