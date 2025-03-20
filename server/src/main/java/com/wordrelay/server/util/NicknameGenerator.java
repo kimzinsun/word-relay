3935e5c99c45cdfc05dc485251360e5a82f2a15f
Kim JinSeon <kwlstjs00@gmail.com>
2025-03-20 13:57:34 +0900

    feat: 랜덤 닉네임 생성 기능

--- server/src/main/java/com/wordrelay/server/util/NicknameGenerator.java
+++ server/src/main/java/com/wordrelay/server/util/NicknameGenerator.java
@@ -1,10 +1,30 @@
 package com.wordrelay.server.util;
 
-import java.util.UUID;
+import com.wordrelay.server.mapper.AdjectivesMapper;
+import com.wordrelay.server.mapper.AnimalsMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
+@Slf4j
 public class NicknameGenerator {
-  public static String generateRandomNickname() {
-    return "Player_" + UUID.randomUUID().toString().substring(0, 5);
-  }
 
+    private final AdjectivesMapper adjectivesMapper;
+    private final AnimalsMapper animalsMapper;
+
+    @Autowired
+    public NicknameGenerator(AdjectivesMapper adjectivesMapper, AnimalsMapper animalsMapper) {
+        this.adjectivesMapper = adjectivesMapper;
+        this.animalsMapper = animalsMapper;
+    }
+
+    public String generateRandomNickname() {
+        int adjectiveIndex = (int) (Math.random() * 145);
+        int animalIndex = (int) (Math.random() * 50);
+
+        String adjective = adjectivesMapper.selectAdjective(adjectiveIndex);
+        String animal = animalsMapper.selectAnimal(animalIndex);
+        return adjective + " " + animal;
+    }
 }
