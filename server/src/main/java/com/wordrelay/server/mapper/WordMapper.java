3ca3bc2bac145e73de3f2cead0524dd858675033
Kim JinSeon <kwlstjs00@gmail.com>
2025-03-23 18:18:33 +0900

    feat: Word 모델 생성

--- server/src/main/java/com/wordrelay/server/mapper/WordMapper.java
+++ server/src/main/java/com/wordrelay/server/mapper/WordMapper.java
@@ -1,10 +1,13 @@
 package com.wordrelay.server.mapper;
 
+import com.wordrelay.server.model.Word;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface WordMapper {
   Boolean selectWord(@Param("tableName") String tableName, @Param("word") String word);
+
+  Word getWord(@Param("tableName") String tableName, @Param("word") String word);
 
 }
